83e971a5dd0eba972eb1e801c17ca5698b614d73
Suraj Patil <52925856+Suraj-S-Patil@users.noreply.github.com>
2020-01-29 22:41:56 +0530

    Add files via upload

---
+++ Simple_Intrst.py
@@ -0,0 +1,7 @@
+#Program to calculate Simple Interest
+print("Enter the principal amount, rate and time period: ")
+princ=int(input())
+rate=float(input())
+time=float(input())
+si=(princ*rate*time)/100
+print(f"The simple interest for given data is: {si}.")
